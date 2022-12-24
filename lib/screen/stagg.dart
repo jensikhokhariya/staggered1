53d4313db31e1a02540e5436a3ef86e76ba98457
jensikhokhariyaflutter <jensikhokhariyaflutter.artonest@gmail.com>
2022-12-24 14:38:55 +0530

    24-12-2022

--- lib/screen/stagg.dart
+++ lib/screen/stagg.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe, camel_case_types
+
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
@@ -12,6 +14,7 @@ class _Stagg_SState extends State<Stagg_S> {
   List images = [
     "assets/images/img1.jpg",
     "assets/images/img2.jpg",
+    "assets/images/img1.jpg",
     "assets/images/img3.webp",
     "assets/images/img4.jpg",
     "assets/images/img1.jpg",
@@ -25,18 +28,28 @@ class _Stagg_SState extends State<Stagg_S> {
     return SafeArea(
       child: Scaffold(
         body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          margin: EdgeInsets.all(10),
-          child: GridView.builder(
-            gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-            itemCount: images.length,
-            itemBuilder: (context, index) {
-              return Image.asset("${images[index]}");
-            },
-          ),
-        ),
+            height: double.infinity,
+            width: double.infinity,
+            margin: EdgeInsets.all(5),
+            child: SingleChildScrollView(
+              child: StaggeredGrid.count(
+                crossAxisCount: 2,
+                children: [
+                  ...List.generate(
+                      images.length, (index) => Padding(padding: EdgeInsets.all(5),child: Image.asset(images[index]))),
+                ],
+              ),
+            )
+
+            // GridView.builder(
+            //   gridDelegate:
+            //       SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            //   itemCount: images.length,
+            //   itemBuilder: (context, index) {
+            //     return Image.asset("${images[index]}");
+            //   },
+            // ),
+            ),
       ),
     );
   }
